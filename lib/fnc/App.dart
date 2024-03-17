1fa6832637f96f8ac4f1610cbee45a6a337a5ba8
Элеонора Середа <sereda.eleonora@mail.ru>
2024-03-17 22:59:45 +1000

    Разбила проект на файлы

---
+++ lib/fnc/App.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:ne_new/UI/homep.dart';
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ForbiddenGram',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'ГеймDevky и GameБойчики'),
+    );
+  }
+}
